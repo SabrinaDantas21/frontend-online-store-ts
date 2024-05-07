fbcd452ee1cc622a453266a7ae7f5a23078bce72
Eliza Sousa <eli.sinais@gmail.com>
2024-05-07 18:03:40 -0300

    Cria rota para a página de detalhes do produto

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './__pages__/HomePage';
 import ShoppingCart from './__pages__/ShoppingCart';
+import DetailsPage from './__pages__/Details';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={ <HomePage /> } />
+      <Route path="/details/:id" element={ <DetailsPage /> } />
       <Route path="/shopping-cart" element={ <ShoppingCart /> } />
     </Routes>
   );
--- src/__components__/Card.tsx
+++ src/__components__/Card.tsx
@@ -1,12 +1,18 @@
+import { Link } from 'react-router-dom';
 import { ProductsType } from '../types';
 
 function Card({ prop }:{ prop: ProductsType }) {
   return (
-    <section data-testid="product">
-      <h2>{ prop.title }</h2>
-      <img src={ prop.thumbnail } alt={ prop.title } />
-      <h4>{ prop.price }</h4>
-    </section>
+    <Link
+      data-testid="product-detail-link"
+      to={ `/details/${prop.id}` }
+    >
+      <section data-testid="product">
+        <h2>{ prop.title }</h2>
+        <img src={ prop.thumbnail } alt={ prop.title } />
+        <h4>{ prop.price }</h4>
+      </section>
+    </Link>
   );
 }
 
