cf2c4c9d75bf0c1af8959670859f4c461955d8c2
b166erobot <bernard0freitas@outlook.com>
2024-05-08 10:01:48 -0300

    Requisito 9 satisfeito

---
+++ src/services/tools.ts
@@ -0,0 +1,19 @@
+import { ProductsType } from '../types';
+
+function addProductCart(prop: ProductsType) {
+  const jsonString = localStorage.getItem('productsCart');
+  const cart: ProductsType[] = jsonString ? JSON.parse(jsonString) : [];
+  const selectedProductIndex = cart.findIndex((item) => item.id === prop.id);
+  const productToAdd = { ...prop, selected_quantity: 1 };
+
+  if (selectedProductIndex !== -1) {
+    cart[selectedProductIndex].selected_quantity = (
+      cart[selectedProductIndex].selected_quantity || 1) + 1;
+  } else {
+    cart.push(productToAdd);
+  }
+
+  localStorage.setItem('productsCart', JSON.stringify(cart));
+}
+
+export { addProductCart };
