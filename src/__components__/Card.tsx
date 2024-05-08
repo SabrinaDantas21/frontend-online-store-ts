53ff31fba3c49c07bb328c17d9c8eac86fce7c46
Bernardo Tomas <bernardotomas.c14@gmail.com>
2024-05-08 08:44:39 -0300

    Resolvendo Linter

--- src/__components__/Card.tsx
+++ src/__components__/Card.tsx
@@ -9,7 +9,7 @@ function Card({ prop }:{ prop: ProductsType }) {
     const cart: ProductsType[] = jsonString ? JSON.parse(jsonString) : [];
     const selectedProductIndex = cart.findIndex((item) => item.id === prop.id);
     const productToAdd = { ...prop, selected_quantity: 1 };
-    
+
     if (selectedProductIndex !== -1) {
       cart[selectedProductIndex].selected_quantity = (
         cart[selectedProductIndex].selected_quantity || 1) + 1;
@@ -30,32 +30,36 @@ function Card({ prop }:{ prop: ProductsType }) {
       <section data-testid="product">
         {
         location.pathname === '/'
-        ? (
-          <>
-            <h2>{ prop.title }</h2>
-            <img src={ prop.thumbnail } alt={ prop.title } />
-            <h4>{ `Preço:R$${prop.price}` }</h4>
-            <label htmlFor="add-to-cart">
-              <input
-                id="add-to-cart"
-                type="checkbox"
-                data-testid="product-add-to-cart"
-                onChange={ addProductCart }
-              />
-              Adicionar ao carrinho
-            </label>
-          </>
-        )
-        : (
-          <>
-            <h2 data-testid="shopping-cart-product-name">{prop.title}</h2>
-            <h4>{ `Preço:R$${prop.price}` }</h4>
-            <h4 data-testid="shopping-cart-product-quantity">{prop.selected_quantity}</h4>
-          </>
-        )
+          ? (
+            <>
+              <h2>{ prop.title }</h2>
+              <img src={ prop.thumbnail } alt={ prop.title } />
+              <h4>{ `Preço:R$${prop.price}` }</h4>
+              <label htmlFor="add-to-cart">
+                <input
+                  id="add-to-cart"
+                  type="checkbox"
+                  data-testid="product-add-to-cart"
+                  onChange={ addProductCart }
+                />
+                Adicionar ao carrinho
+              </label>
+            </>
+          )
+          : (
+            <>
+              <h2 data-testid="shopping-cart-product-name">{prop.title}</h2>
+              <h4>{ `Preço:R$${prop.price}` }</h4>
+              <h4
+                data-testid="shopping-cart-product-quantity"
+              >
+                {prop.selected_quantity}
+              </h4>
+            </>
+          )
       }
       </section>
-   </Link>
+    </Link>
   );
 }
 
