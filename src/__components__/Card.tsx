2be80327d51140eeb0d94f641acf9d483ea68980
SabrinaDantas21 <brinahdantas56@gmail.com>
2024-05-07 21:33:03 -0300

    requisito 8 quase finalizado

--- src/__components__/Card.tsx
+++ src/__components__/Card.tsx
@@ -1,3 +1,4 @@
+import { useLocation } from 'react-router-dom';
 import { ProductsType } from '../types';
 
 function Card({ prop }:{ prop: ProductsType }) {
@@ -5,30 +6,45 @@ function Card({ prop }:{ prop: ProductsType }) {
   function addProductCart(event: React.ChangeEvent<HTMLInputElement>) {
     let newCart;
     const jsonString = localStorage.getItem('productsCart');
-    const cart: string[] = jsonString ? JSON.parse(jsonString as string) : [];
+    const cart: ProductsType[] = jsonString ? JSON.parse(jsonString as string) : [];
     if (!event.target.checked) {
-      newCart = cart.filter((item) => item !== title);
+      newCart = cart.filter((item) => item.title !== title);
       localStorage.setItem('productsCart', JSON.stringify(newCart));
     } else {
-      cart.push(title);
+      cart.push(prop);
       localStorage.setItem('productsCart', JSON.stringify(cart));
     }
   }
+  const location = useLocation();
 
   return (
     <section data-testid="product">
-      <h2>{ prop.title }</h2>
-      <img src={ prop.thumbnail } alt={ prop.title } />
-      <h4>{ `Preço:R$${prop.price}` }</h4>
-      <label htmlFor="add-to-cart">
-        <input
-          id="add-to-cart"
-          type="checkbox"
-          data-testid="product-add-to-cart"
-          onChange={ addProductCart }
-        />
-        Adicionar ao carrinho
-      </label>
+      {
+    location.pathname === '/'
+      ? (
+        <>
+          <h2>{ prop.title }</h2>
+          <img src={ prop.thumbnail } alt={ prop.title } />
+          <h4>{ `Preço:R$${prop.price}` }</h4>
+          <label htmlFor="add-to-cart">
+            <input
+              id="add-to-cart"
+              type="checkbox"
+              data-testid="product-add-to-cart"
+              onChange={ addProductCart }
+            />
+            Adicionar ao carrinho
+          </label>
+        </>
+      )
+      : (
+        <>
+          <h2 data-testid="shopping-cart-product-name">{prop.title}</h2>
+          <h4>{ `Preço:R$${prop.price}` }</h4>
+          <h4 data-testid="shopping-cart-product-quantity">{prop.available_quantity}</h4>
+        </>
+      )
+  }
     </section>
   );
 }
--- src/__pages__/ShoppingCart.tsx
+++ src/__pages__/ShoppingCart.tsx
@@ -3,9 +3,10 @@ import { FaShoppingCart } from 'react-icons/fa';
 import { TiArrowBack } from 'react-icons/ti';
 import { Link } from 'react-router-dom';
 import Card from '../__components__/Card';
+import { ProductsType } from '../types';
 
 export default function ShoppingCart() {
-  const [cart, setCart] = useState<string[] | []>([]);
+  const [cart, setCart] = useState<ProductsType[] | []>([]);
 
   useEffect(() => {
     const jsonString = localStorage.getItem('productsCart');
@@ -21,14 +22,18 @@ export default function ShoppingCart() {
       </h2>
       <Link to="/"><TiArrowBack /></Link>
       {
-        !cart
+        !cart.length
           ? (
             <p data-testid="shopping-cart-empty-message">
               Seu carrinho está vazio
             </p>
           )
-          : <h2>produtos no carrinho</h2>
-}
+          : cart.map((element, index) => (<Card
+              key={ index }
+              prop={ element }
+          />
+          ))
+            }
     </div>
   );
 }
